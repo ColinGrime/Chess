16e753be4bde18e851f204612728cdebc92081b1
Scill <realscill@gmail.com>
2022-02-08 20:20:09 -0500

    Fixed Pawn initial move, so it cannot move 2 spaces if it's attacking

--- src/main/java/me/scill/chess/pieces/Pawn.java
+++ src/main/java/me/scill/chess/pieces/Pawn.java
@@ -20,14 +20,14 @@ public class Pawn extends Piece {
 		// The move has a piece on it.
 		if (tile.getPiece() != null)
 			// Returns true if it moves to the side in the correct direction.
-			return columnDiff == 1 && isRightDirection(fromRow, toRow);
+			return columnDiff == 1 && isRightDirection(fromRow, toRow, true);
 
 		// Pawns can't move to the side!
 		if (columnDiff >= 1)
 			return false;
 
 		// The move is valid.
-		return isRightDirection(fromRow, toRow);
+		return isRightDirection(fromRow, toRow, false);
 	}
 
 	@Override
@@ -39,15 +39,23 @@ public class Pawn extends Piece {
 	 * White pieces move up.
 	 * Black pieces move down.
 	 */
-	private boolean isRightDirection(int fromRow, int toRow) {
+	private boolean isRightDirection(int fromRow, int toRow, boolean isAttacking) {
 		// Gets how far the Pawn has moved depending on its side.
 		int diff = getSide() == Side.WHITE ? toRow - fromRow : fromRow - toRow;
+		boolean isRight = false;
 
-		if (diff == 2 && isFirstMove) {
+		// Pawns can move 2 spaces on the first turn if it's normal.
+		if (!isAttacking && diff == 2 && isFirstMove)
+			isRight = true;
+
+		// Pawns can always move 1 space in the right direction!
+		else if (diff == 1)
+			isRight = true;
+
+		// The direction is right, so it moves.
+		if (isFirstMove && isRight)
 			isFirstMove = false;
-			return true;
-		}
 
-		return diff == 1;
+		return isRight;
 	}
 }
