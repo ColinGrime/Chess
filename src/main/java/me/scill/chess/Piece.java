48e74431ce465a3d5d3e7d747941007e3302e406
Scill <realscill@gmail.com>
2022-02-08 19:39:05 -0500

    Updated isValidPlay(...) to check for blocked moves

--- src/main/java/me/scill/chess/Piece.java
+++ src/main/java/me/scill/chess/Piece.java
@@ -5,34 +5,55 @@ import me.scill.chess.display.SquareTile;
 public abstract class Piece {
 
 	private final Side side;
-	private SquareTile position;
+	private SquareTile tile;
 
 	public Piece(Side side) {
 		this.side = side;
 	}
 
-	public boolean isValidPlay(SquareTile position) {
+	public boolean isValidPlay(SquareTile tile) {
+		// Row and column differences
+		int rowDiff = Math.abs(tile.getRowPos() - getTile().getRowPos());
+		int columnDiff = Math.abs(tile.getColumnPos() - getTile().getColumnPos());
+
+		// Row lowest and highest indexes.
+		int[] rowIndex = new int[] {
+				Math.min(tile.getRowPos(), getTile().getRowPos()),
+				Math.max(tile.getRowPos(), getTile().getRowPos())
+		};
+
+		// Column lowest and highest indexes.
+		int[] columnIndex = new int[] {
+				Math.min(tile.getColumnPos(), getTile().getColumnPos()),
+				Math.max(tile.getColumnPos(), getTile().getColumnPos())
+		};
+
+		// Check if the Piece is blocked.
+		if (isBlocked(tile, rowDiff, columnDiff, rowIndex, columnIndex))
+			return false;
+
 		// Checks if the Piece can move to that square.
-		if (!isValidMove(position))
+		if (!isValidMove(tile, rowDiff, columnDiff))
 			return false;
 
 		// Does the square have a Piece on it?
-		if (position.getPiece() != null)
+		if (tile.getPiece() != null)
 			// If so, make sure they're on different sides.
-			return side != position.getPiece().getSide();
+			return side != tile.getPiece().getSide();
 
-		// Square is valid.
+		// The play is valid.
 		return true;
 	}
 
-	public abstract boolean isValidMove(SquareTile position);
+	public abstract boolean isValidMove(SquareTile tile, int rowDiff, int columnDiff);
+	public abstract boolean isBlocked(SquareTile tile, int rowDiff, int columnDiff, int[] rowIndex, int[] columnIndex);
 
-	public SquareTile getPosition() {
-		return position;
+	public SquareTile getTile() {
+		return tile;
 	}
 
-	public void setPosition(SquareTile position) {
-		this.position = position;
+	public void setTile(SquareTile tile) {
+		this.tile = tile;
 	}
 
 	public Side getSide() {
