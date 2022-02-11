54199a4d85feae136a5ffeec365f6e15f3aa4767
Scill <realscill@gmail.com>
2022-02-10 21:38:29 -0500

    Fixed highlighting and added turns

--- src/main/java/me/scill/chess/display/Tile.java
+++ src/main/java/me/scill/chess/display/Tile.java
@@ -1,7 +1,6 @@
 package me.scill.chess.display;
 
-import me.scill.chess.Piece;
-import me.scill.chess.StretchIcon;
+import me.scill.chess.board.Piece;
 import me.scill.chess.board.Board;
 
 import javax.imageio.ImageIO;
@@ -13,10 +12,10 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
 
-public class SquareTile extends JButton implements ActionListener {
+public class Tile extends JButton implements ActionListener {
 
 
-	private static SquareTile selectedSquare = null;
+	private static Tile selectedTile = null;
 	private static Color selectedColor = null;
 
 	private final Board board;
@@ -24,11 +23,11 @@ public class SquareTile extends JButton implements ActionListener {
 	private final char columnPos;
 	private Piece piece;
 
-	public SquareTile(Board board, int rowPos, char columnPos) {
+	public Tile(Board board, int rowPos, char columnPos) {
 		this(board, rowPos, columnPos, Color.WHITE);
 	}
 
-	public SquareTile(Board board, int rowPos, char columnPos, Color color) {
+	public Tile(Board board, int rowPos, char columnPos, Color color) {
 		this.board = board;
 		this.rowPos = rowPos;
 		this.columnPos = columnPos;
@@ -41,62 +40,68 @@ public class SquareTile extends JButton implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// One of the squares is selected.
-		if (selectedSquare != null) {
+		// One of the tiles is selected.
+		if (selectedTile != null) {
 			boolean isAlly = false;
+			boolean shouldHighlight = selectedTile != this;
 
-			// If the clicked-on square has a Piece that exists, check if it's an ally.
-			if (selectedSquare != this && getPiece() != null)
-				isAlly = selectedSquare.getPiece().getSide() == getPiece().getSide();
+			// If the clicked-on tile has a Piece that exists, check if it's an ally.
+			if (getPiece() != null)
+				isAlly = selectedTile.getPiece().getSide() == getPiece().getSide();
 
 			// If's a valid move, play it.
-			if (!isAlly && selectedSquare.getPiece().isValidPlay(this)) {
-				setPiece(selectedSquare.getPiece());
-				selectedSquare.setPiece(null);
+			if (!isAlly && selectedTile.getPiece().isValidPlay(this)) {
+				setPiece(selectedTile.getPiece());
+				selectedTile.setPiece(null);
+
+				board.nextTurn();
+				shouldHighlight = false;
 			}
 
-			// Removes the highlight and de-selects the selected square.
-			selectedSquare.setBackground(selectedColor);
-			selectedSquare = null;
+			// Removes the highlight and de-selects the selected tile.
+			selectedTile.setBackground(selectedColor);
+			selectedTile = null;
 
-			// Return if the clicked-on square is NOT an ally.
-			if (!isAlly)
+			// Return if the clicked-on tile shouldn't be highlighted.
+			if (!shouldHighlight)
 				return;
 		}
 
-		// If the clicked-on square has a Piece that exists, select it.
-		if (getPiece() != null) {
-			selectedSquare = this;
+		// If the clicked-on tile has a Piece that exists, select it.
+		if (getPiece() != null && getPiece().getSide() == board.getCurrentTurn()) {
+			selectedTile = this;
 			selectedColor = getBackground();
 			setBackground(Color.YELLOW);
 		}
-	}
-
-	public Board getBoard() {
-		return board;
-	}
-
-	public int getRowPos() {
-		return rowPos;
-	}
-
-	public char getColumnPos() {
-		return columnPos;
 	}
 
 	public Piece getPiece() {
 		return piece;
 	}
 
+	/**
+	 * Sets the chess piece on the tile.
+	 * @param piece any chess piece
+	 */
 	public void setPiece(Piece piece) {
 		this.piece = piece;
+		paintPiece(piece);
 
+		// If the chess piece exists, set its tile to this tile.
+		if (piece != null)
+			piece.setTile(this);
+	}
+
+	/**
+	 * Paints the chess icon onto the tile.
+	 * @param piece any chess piece
+	 */
+	private void paintPiece(Piece piece) {
 		if (piece == null) {
 			setIcon(null);
 			return;
 		}
 
-		piece.setTile(this);
 		BufferedImage bufferedImage = null;
 
 		try {
@@ -122,6 +127,18 @@ public class SquareTile extends JButton implements ActionListener {
 		Image image = bufferedImage.getScaledInstance(90, 90, Image.SCALE_SMOOTH);
 		SwingUtilities.invokeLater(() -> setIcon(new StretchIcon(image)));
 		SwingUtilities.invokeLater(() -> setPressedIcon(new StretchIcon(image)));
+	}
+
+	public Board getBoard() {
+		return board;
+	}
+
+	public int getRowPos() {
+		return rowPos;
+	}
+
+	public char getColumnPos() {
+		return columnPos;
 	}
 
 	@Override
