1dd2f3ef94522de7add13a03055bde02ad2d7311
Scill <realscill@gmail.com>
2022-02-14 00:44:51 -0500

    Initial commit

---
+++ src/main/java/me/scill/chess/Music.java
@@ -0,0 +1,82 @@
+package me.scill.chess;
+
+import me.scill.chess.enums.Song;
+import me.scill.chess.utilities.ResourceUtility;
+
+import javax.sound.sampled.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Random;
+import java.util.concurrent.CountDownLatch;
+
+public class Music extends Thread {
+
+	public enum Status {
+		HOME,
+		NORMAL,
+		CLIMAX,
+	}
+
+	private final Random rng = new Random();
+	private final Song[] HOME_MUSIC = { Song.DevilPiano, Song.BeforeTheFight, Song.Lunokhod };
+	private final Song[] GAME_MUSIC = { Song.ImpatientlyWaiting, Song.OrNot };
+	private final Song[] INTENSE_MUSIC = { Song.ThePercussionist, Song.ThinWallsPercussion };
+	private Status status = Status.HOME;
+
+	@Override
+	public void run() {
+		playMusic(null);
+	}
+
+	public void playMusic(Song justPlayed) {
+		Song song = switch (status) {
+			case HOME -> HOME_MUSIC[rng.nextInt(HOME_MUSIC.length)];
+			case NORMAL -> GAME_MUSIC[rng.nextInt(GAME_MUSIC.length)];
+			case CLIMAX -> INTENSE_MUSIC[rng.nextInt(INTENSE_MUSIC.length)];
+		};
+
+		// If the song just played, find another.
+		if (song == justPlayed) {
+			playMusic(justPlayed);
+			return;
+		}
+
+		playSong(song);
+	}
+
+	private void playSong(Song song) {
+		// Open an audio input stream.
+		InputStream inputStream = ResourceUtility.getStream(song.path());
+		CountDownLatch syncLatch = new CountDownLatch(1);
+
+		try (AudioInputStream audioIn = AudioSystem.getAudioInputStream(inputStream)) {
+			// Get a sound clip resource.
+			Clip clip = AudioSystem.getClip();
+
+			// When the song is finished, it finds another song to play!
+			clip.addLineListener(e -> {
+				if (e.getType() == LineEvent.Type.STOP) {
+					clip.close();
+					syncLatch.countDown();
+				}
+			});
+
+			// Open audio clip and start playing.
+			clip.open(audioIn);
+			clip.start();
+		} catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+			e.printStackTrace();
+		}
+
+		try {
+			syncLatch.await();
+			playMusic(song);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+}
