992581248d524c59985227558b80adf18112cc79
Scill <realscill@gmail.com>
2022-03-13 23:03:37 -0400

    Add note

--- src/main/java/me/colin/chess/Chess.java
+++ src/main/java/me/colin/chess/Chess.java
@@ -2,10 +2,19 @@ package me.colin.chess;
 
 import me.colin.chess.display.Display;
 
-/*
+/**
  * Created by: Colin Grimes
- *
  * Play some chess! :)
+ *
+ * --IMPORTANT NOTE TO MAKE--
+ * To assist in developing, I used the {@link me.colin.chess.display.StretchIcon} class.
+ * It allows you to dynamically re-size images while keeping the quality up.
+ *
+ * It's about 20-30 lines of actual code.
+ * So instead of having choppy images (and so I don't have to do math), I decided to use it.
+ *
+ * I can easily remove it if needed, but I point it out since it's copied code.
+ * Thanks~
  */
 public class Chess {
 
